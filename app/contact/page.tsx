dadaa106074665c0fd4826208948a16893123d41
Alfredo Flores <alfredofloresgarcia156@gmail.com>
2023-11-04 14:05:47 -0600

    Add CodeDrifter info, hide projects for now

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,27 +1,27 @@
 "use client";
-import { Github, Mail, Twitter } from "lucide-react";
+import {Github, Linkedin, Mail} from "lucide-react";
 import Link from "next/link";
 import { Navigation } from "../components/nav";
 import { Card } from "../components/card";
 
 const socials = [
 	{
-		icon: <Twitter size={20} />,
-		href: "https://twitter.com/chronark_",
-		label: "Twitter",
-		handle: "@chronark_",
+		icon: <Linkedin size={20} />,
+		href: "https://www.linkedin.com/in/alfredo-flores-mx",
+		label: "LinkedIn",
+		handle: "alfredo-flores-mx",
 	},
 	{
 		icon: <Mail size={20} />,
-		href: "mailto:dev@chronark.com",
+		href: "mailto:contact@alfredoflores.dev",
 		label: "Email",
-		handle: "dev@chronark.com",
+		handle: "contact@alfredoflores.dev",
 	},
 	{
 		icon: <Github size={20} />,
-		href: "https://github.com/chronark",
+		href: "https://github.com/codedrifter-mx",
 		label: "Github",
-		handle: "chronark",
+		handle: "codedrifter-mx",
 	},
 ];
 
